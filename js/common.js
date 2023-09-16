b2b18703fc535f1c4b734b4e20a20002b2b7115b
Tatsu <nekonekokoneko788@gmail.com>
2023-09-17 04:15:19 +0900

    modify css index.html

--- js/common.js
+++ js/common.js
@@ -18,3 +18,64 @@ $(function () {
     });
   }
 });
+
+// スライドショ-
+$('.slider').slick({
+  autoplaySpeed: 2500,
+  speed: 1200,
+  autoplay: true,       
+  autoplaySpeed: 2000,        
+  dots: false,          
+  arrows: false,        
+  infinite: true,       
+});
+
+const loadAnime = $('.spinner-box');
+$(window).on('load', function() {
+    loadAnime.delay(500).fadeOut(1000);
+});
+
+function stopLoad() {
+    loadAnime.delay(1000).fadeOut(700);
+}
+setTimeout('stopLoad()', 10000);
+
+// フェードイン
+function fadeIn() {
+  $('.fadeUpTrigger').each(function() {
+    let scroll = $(window).scrollTop();
+    let triTop = $(this).offset().top + 100;
+    let winHeight = $(window).height();
+    if (scroll >= triTop - winHeight) {
+      $(this).addClass('fadeUp');
+    }else {
+      $(this).removeClass('fadeUp');
+    }
+  });
+
+  $('.fadeLeftTrigger').each(function() {
+    let scroll = $(window).scrollTop();
+    let triTop = $(this).offset().top + 100;
+    let winHeight = $(window).height();
+    if (scroll >= triTop - winHeight) {
+      $(this).addClass('fadeLeft');
+    }else {
+      $(this).removeClass('fadeLeft');
+    }
+  });
+
+  $('.fadeRightTrigger').each(function() {
+    let scroll = $(window).scrollTop();
+    let triTop = $(this).offset().top + 100;
+    let winHeight = $(window).height();
+    if (scroll >= triTop - winHeight) {
+      $(this).addClass('fadeRight');
+    }else {
+      $(this).removeClass('fadeRight');
+    }
+  });
+}
+
+$(window).scroll(function() {
+  fadeIn();
+});
